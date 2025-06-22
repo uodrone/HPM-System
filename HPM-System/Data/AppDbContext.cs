abaa4956837e203350982ccf1472c300150db82a
uodrone <drone_86@mail.ru>
2025-06-22 12:51:25 +0400

    Отправка настроек EF Core

---
+++ HPM-System/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HPM_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPM_System.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PersonRole> PersonRoles { get; set; }
+    }
+}
---
+++ HPM-System/Models/PersonRole.cs
@@ -0,0 +1,9 @@
+﻿namespace HPM_System.Models
+{
+    public class PersonRole
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
