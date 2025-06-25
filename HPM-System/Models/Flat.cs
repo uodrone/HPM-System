acac9fe059de24f408349aaffd5bb8180ffaa7d9
ElviraAkbasheva <165509920+ElviraAkbasheva@users.noreply.github.com>
2025-06-25 20:59:02 +0300

    Added flat, person classes
    Added flat, person classes

---
+++ HPM-System/Models/Flat.cs
@@ -0,0 +1,21 @@
+﻿namespace HPM_System.Models
+{
+    public class Flat
+    {
+        public int ID { get; set; }
+        public double Area { get; set; }
+        public int Resident { get; set; }
+        public byte Entrance { get; set; }
+        public int Floor { get; set; }
+
+        public Flat(int iD, double area, int resident, byte entrance, int floor)
+        {
+            ID = iD;
+            Area = area;
+            Resident = resident;
+            Entrance = entrance;
+            Floor = floor;
+        }
+    }
+}
+
