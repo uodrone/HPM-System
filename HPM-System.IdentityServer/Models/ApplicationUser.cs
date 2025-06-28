571e9d4f78127d3a8a482b96840b1710e123f824
uodrone <drone_86@mail.ru>
2025-06-28 11:54:54 +0400

    Исправление регистрации, оказалось что модель была не та и переопределяла базовые значения модели IdentityUser

--- HPM-System.IdentityServer/Models/ApplicationUser.cs
+++ HPM-System.IdentityServer/Models/ApplicationUser.cs
@@ -7,7 +7,5 @@ namespace HPM_System.IdentityServer.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string? Patronymic { get; set; }
-        public string? Email { get; set; }
-        public string PhoneNumber { get; set; }
     }
 }
