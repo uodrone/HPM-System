1977d5f39f6db04ca6cd7744a1072c204bf0b66e
ElviraAkbasheva <165509920+ElviraAkbasheva@users.noreply.github.com>
2025-07-07 19:55:58 +0300

    Коммит перед Develop -> Building

---
+++ HPM-System/Models/Building.cs
@@ -0,0 +1,19 @@
+﻿namespace HPM_System.Models
+{
+    public class Building
+    {
+        public int Id { get; set; }
+        public BuildingType Type { get; set; }
+        public int NumberBuilding { get; set; } 
+        public double AreaBuilding { get; set; } // площадь самого дома 
+        public double AreaLiving { get; set; } // жилая площадь дома
+        public double AreaLand { get; set; } // площадь придомовой территории 
+        public int Floors { get; set; } // этажность 
+        public int Entrances { get; set; }  // количество подъездов
+        public bool HasGas { get; set; }
+        public bool HasElectricity { get; set; }
+        public bool HasElevator { get; set; } 
+        public ApplicationUser Head { get; set; } // старший по дому
+        public ICollection<Apartment> ApartmentsList { get; set; } =  new List<Apartment>();
+    }
+}
---
+++ HPM-System/Models/BuildingType.cs
@@ -0,0 +1,9 @@
+﻿namespace HPM_System.Models
+{
+    public class BuildingType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; } // многоквартирный - block; индивидуальный - house
+        public string Description { get; set; }
+    }
+}
