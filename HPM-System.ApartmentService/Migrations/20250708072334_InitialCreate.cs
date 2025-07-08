7294030c0a289fc1c8929a3f59f957420ed9b3d6
uodrone <drone_86@mail.ru>
2025-07-08 11:25:49 +0400

    Нормально все сделал

---
+++ HPM-System.ApartmentService/Migrations/20250708072334_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HPMSystem.ApartmentService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Apartment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CommunityId = table.Column<int>(type: "integer", nullable: false),
+                    Number = table.Column<int>(type: "integer", nullable: false),
+                    NumbersOfRooms = table.Column<int>(type: "integer", nullable: false),
+                    ResidentialArea = table.Column<decimal>(type: "numeric", nullable: false),
+                    TotalArea = table.Column<decimal>(type: "numeric", nullable: false),
+                    Floor = table.Column<int>(type: "integer", nullable: true),
+                    UserId = table.Column<List<int>>(type: "integer[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Apartment", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apartment_CommunityId",
+                table: "Apartment",
+                column: "CommunityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Apartment");
+        }
+    }
+}
