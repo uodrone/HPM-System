a52e503c0503096ea4af3414bc38fc8d64ab234a
uodrone <drone_86@mail.ru>
2025-09-21 11:44:19 +0400

    Модель дома

---
+++ HPM-System.ApartmentService/Models/House.cs
@@ -0,0 +1,22 @@
+﻿namespace HPM_System.ApartmentService.Models
+{
+    public class House
+    {
+        long Id { get; set; }
+        string City { get; set; }
+        string Street { get; set; }
+        string Number { get; set; }
+        public int Entrances { get; set; }
+        public int Floors { get; set; }
+        public bool HasGas { get; set; }
+        public bool HasElectricity { get; set; }
+        public bool HasElevator { get; set; }
+        public Guid? HeadId { get; set; }
+        public string? PostIndex { get; set; }
+        public double? ApartmentsArea { get; set; }
+        public double? TotalArea { get; set; }
+        public double? LandArea { get; set; }
+        public ICollection<Apartment>? Apartments { get; set; }
+
+    }
+}
