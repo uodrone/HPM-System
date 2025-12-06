19e7ee7dfdfe66a3645f894ce4f8333425e834a2
uodrone <drone_86@mail.ru>
2025-12-06 20:55:18 +0400

    Свод миграций, надо настроить compose

---
+++ HPM-System.UserService/Migrations/20250905031436_UserBirthday.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HPM_System.UserService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserBirthday : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Age",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "Users",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
