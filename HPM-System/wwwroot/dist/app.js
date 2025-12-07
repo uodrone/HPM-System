d53d7ff4d48e3bde13aa751c612a1e5f34441dc0
uodrone <drone_86@mail.ru>
2025-12-07 23:02:36 +0400

    Начало работ по фронту событий

--- HPM-System/wwwroot/dist/app.js
+++ HPM-System/wwwroot/dist/app.js
@@ -3792,19 +3792,185 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _FileStorageClient_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./FileStorageClient.js */ "./wwwroot/js/FileStorageClient.js");
 /* harmony import */ var _DateFormat_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./DateFormat.js */ "./wwwroot/js/DateFormat.js");
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _regenerator() { /*! regenerator-runtime -- Copyright (c) 2014-present, Facebook, Inc. -- license (MIT): https://github.com/babel/babel/blob/main/packages/babel-helpers/LICENSE */ var e, t, r = "function" == typeof Symbol ? Symbol : {}, n = r.iterator || "@@iterator", o = r.toStringTag || "@@toStringTag"; function i(r, n, o, i) { var c = n && n.prototype instanceof Generator ? n : Generator, u = Object.create(c.prototype); return _regeneratorDefine2(u, "_invoke", function (r, n, o) { var i, c, u, f = 0, p = o || [], y = !1, G = { p: 0, n: 0, v: e, a: d, f: d.bind(e, 4), d: function d(t, r) { return i = t, c = 0, u = e, G.n = r, a; } }; function d(r, n) { for (c = r, u = n, t = 0; !y && f && !o && t < p.length; t++) { var o, i = p[t], d = G.p, l = i[2]; r > 3 ? (o = l === n) && (u = i[(c = i[4]) ? 5 : (c = 3, 3)], i[4] = i[5] = e) : i[0] <= d && ((o = r < 2 && d < i[1]) ? (c = 0, G.v = n, G.n = i[1]) : d < l && (o = r < 3 || i[0] > n || n > l) && (i[4] = r, i[5] = n, G.n = l, c = 0)); } if (o || r > 1) return a; throw y = !0, n; } return function (o, p, l) { if (f > 1) throw TypeError("Generator is already running"); for (y && 1 === p && d(p, l), c = p, u = l; (t = c < 2 ? e : u) || !y;) { i || (c ? c < 3 ? (c > 1 && (G.n = -1), d(c, u)) : G.n = u : G.v = u); try { if (f = 2, i) { if (c || (o = "next"), t = i[o]) { if (!(t = t.call(i, u))) throw TypeError("iterator result is not an object"); if (!t.done) return t; u = t.value, c < 2 && (c = 0); } else 1 === c && (t = i["return"]) && t.call(i), c < 2 && (u = TypeError("The iterator does not provide a '" + o + "' method"), c = 1); i = e; } else if ((t = (y = G.n < 0) ? u : r.call(n, G)) !== a) break; } catch (t) { i = e, c = 1, u = t; } finally { f = 1; } } return { value: t, done: y }; }; }(r, o, i), !0), u; } var a = {}; function Generator() {} function GeneratorFunction() {} function GeneratorFunctionPrototype() {} t = Object.getPrototypeOf; var c = [][n] ? t(t([][n]())) : (_regeneratorDefine2(t = {}, n, function () { return this; }), t), u = GeneratorFunctionPrototype.prototype = Generator.prototype = Object.create(c); function f(e) { return Object.setPrototypeOf ? Object.setPrototypeOf(e, GeneratorFunctionPrototype) : (e.__proto__ = GeneratorFunctionPrototype, _regeneratorDefine2(e, o, "GeneratorFunction")), e.prototype = Object.create(u), e; } return GeneratorFunction.prototype = GeneratorFunctionPrototype, _regeneratorDefine2(u, "constructor", GeneratorFunctionPrototype), _regeneratorDefine2(GeneratorFunctionPrototype, "constructor", GeneratorFunction), GeneratorFunction.displayName = "GeneratorFunction", _regeneratorDefine2(GeneratorFunctionPrototype, o, "GeneratorFunction"), _regeneratorDefine2(u), _regeneratorDefine2(u, o, "Generator"), _regeneratorDefine2(u, n, function () { return this; }), _regeneratorDefine2(u, "toString", function () { return "[object Generator]"; }), (_regenerator = function _regenerator() { return { w: i, m: f }; })(); }
+function _regeneratorDefine2(e, r, n, t) { var i = Object.defineProperty; try { i({}, "", {}); } catch (e) { i = 0; } _regeneratorDefine2 = function _regeneratorDefine(e, r, n, t) { function o(r, n) { _regeneratorDefine2(e, r, function (e) { return this._invoke(r, n, e); }); } r ? i ? i(e, r, { value: n, enumerable: !t, configurable: !t, writable: !t }) : e[r] = n : (o("next", 0), o("throw", 1), o("return", 2)); }, _regeneratorDefine2(e, r, n, t); }
+function asyncGeneratorStep(n, t, e, r, o, a, c) { try { var i = n[a](c), u = i.value; } catch (n) { return void e(n); } i.done ? t(u) : Promise.resolve(u).then(r, o); }
+function _asyncToGenerator(n) { return function () { var t = this, e = arguments; return new Promise(function (r, o) { var a = n.apply(t, e); function _next(n) { asyncGeneratorStep(a, r, o, _next, _throw, "next", n); } function _throw(n) { asyncGeneratorStep(a, r, o, _next, _throw, "throw", n); } _next(void 0); }); }; }
+function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
 function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
 function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
 
 
 
 
 
-var NotificationProfileManager = /*#__PURE__*/_createClass(function NotificationProfileManager() {
-  _classCallCheck(this, NotificationProfileManager);
-});
+var NotificationProfileManager = /*#__PURE__*/function () {
+  function NotificationProfileManager() {
+    _classCallCheck(this, NotificationProfileManager);
+    this.houseProfile = new _ApartmentHouses_js__WEBPACK_IMPORTED_MODULE_1__.ApartmentHouses();
+    this.userId = window.authManager.userData.userId;
+  }
+  return _createClass(NotificationProfileManager, [{
+    key: "InsertDataToCreateEvent",
+    value: function () {
+      var _InsertDataToCreateEvent = _asyncToGenerator(/*#__PURE__*/_regenerator().m(function _callee2() {
+        var _this = this;
+        var houseSelector, eventGroup, houses, houseChecks, results, eligibleHouses;
+        return _regenerator().w(function (_context2) {
+          while (1) switch (_context2.n) {
+            case 0:
+              houseSelector = document.getElementById('houseId');
+              eventGroup = document.querySelector('.profile-group[data-group="event"]'); // Очистим селектор перед заполнением
+              houseSelector.innerHTML = '';
+              _context2.n = 1;
+              return this.houseProfile.GetHousesByUserId(this.userId);
+            case 1:
+              houses = _context2.v;
+              _context2.n = 2;
+              return Promise.all(
+              // Массив промисов для параллельного выполнения
+              houses.map(/*#__PURE__*/function () {
+                var _ref = _asyncToGenerator(/*#__PURE__*/_regenerator().m(function _callee(house) {
+                  var houseHead;
+                  return _regenerator().w(function (_context) {
+                    while (1) switch (_context.n) {
+                      case 0:
+                        _context.n = 1;
+                        return _this.houseProfile.GetHead(house.id);
+                      case 1:
+                        houseHead = _context.v;
+                        if (!(houseHead == null)) {
+                          _context.n = 2;
+                          break;
+                        }
+                        return _context.a(2, null);
+                      case 2:
+                        return _context.a(2, {
+                          house: house,
+                          isHead: houseHead.id === _this.userId
+                        });
+                    }
+                  }, _callee);
+                }));
+                return function (_x) {
+                  return _ref.apply(this, arguments);
+                };
+              }()));
+            case 2:
+              houseChecks = _context2.v;
+              // Фильтруем только ненулевые значения, т.е. там где есть старший по дому
+              results = houseChecks.filter(function (item) {
+                return item !== null;
+              }); // Фильтруем дома, где пользователь — глава
+              eligibleHouses = results.filter(function (_ref2) {
+                var isHead = _ref2.isHead;
+                return isHead;
+              }).map(function (_ref3) {
+                var house = _ref3.house;
+                return house;
+              });
+              if (eligibleHouses.length) {
+                // Пользователь — глава хотя бы в одном доме => показываем селект
+                eligibleHouses.forEach(function (house) {
+                  var option = document.createElement('option');
+                  option.value = house.id;
+                  option.textContent = "".concat(house.city, ", \u0443\u043B. ").concat(house.street, ", ").concat(house.number);
+                  houseSelector.appendChild(option);
+                });
+              } else {
+                // Ни в одном доме не глава => событие недоступно
+                if (eventGroup) {
+                  eventGroup.innerHTML = "\u0421\u043E\u0437\u0434\u0430\u043D\u0438\u0435 \u0441\u043E\u0431\u044B\u0442\u0438\u044F \u043D\u0435\u0434\u043E\u0441\u0442\u0443\u043F\u043D\u043E";
+                }
+              }
+            case 3:
+              return _context2.a(2);
+          }
+        }, _callee2, this);
+      }));
+      function InsertDataToCreateEvent() {
+        return _InsertDataToCreateEvent.apply(this, arguments);
+      }
+      return InsertDataToCreateEvent;
+    }()
+  }]);
+}();
+document.addEventListener('authStateChanged', /*#__PURE__*/_asyncToGenerator(/*#__PURE__*/_regenerator().m(function _callee4() {
+  var _event$detail, isAuthenticated, userData, Regex, UrlParts, userId, eventProfile, eventClient, eventByUser, eventsByUser;
+  return _regenerator().w(function (_context4) {
+    while (1) switch (_context4.n) {
+      case 0:
+        _event$detail = event.detail, isAuthenticated = _event$detail.isAuthenticated, userData = _event$detail.userData;
+        Regex = new window.RegularExtension();
+        UrlParts = Regex.getUrlPathParts(window.location.href);
+        if (!(isAuthenticated && userData)) {
+          _context4.n = 6;
+          break;
+        }
+        userId = window.authManager.userData.userId;
+        eventProfile = new NotificationProfileManager();
+        eventClient = new _EventClient_js__WEBPACK_IMPORTED_MODULE_2__.EventClient();
+        console.log('Аутентификация пройдена');
+        if (!window.location.pathname.includes('/event/create')) {
+          _context4.n = 2;
+          break;
+        }
+        _context4.n = 1;
+        return eventProfile.InsertDataToCreateEvent();
+      case 1:
+        document.querySelector('[data-action="save-event-data"]').addEventListener('click', /*#__PURE__*/_asyncToGenerator(/*#__PURE__*/_regenerator().m(function _callee3() {
+          var eventData, eventCreate;
+          return _regenerator().w(function (_context3) {
+            while (1) switch (_context3.n) {
+              case 0:
+                console.log('Клик по кнопке сохранения уведомления');
+
+                // Собираем данные уведомления
+                _context3.n = 1;
+                return eventProfile.CollectEventDataToCreate();
+              case 1:
+                eventData = _context3.v;
+                console.log('Данные для сохранения:', eventData);
+
+                //Отправляем данные на сервер
+                eventCreate = eventClient.CreateEvent(eventData);
+                if (eventCreate) {
+                  _Modal_js__WEBPACK_IMPORTED_MODULE_0__.Modal.ShowNotification('Событие создано успешно!', 'green');
+                }
+              case 2:
+                return _context3.a(2);
+            }
+          }, _callee3);
+        })));
+      case 2:
+        if (!(window.location.pathname == '/')) {
+          _context4.n = 4;
+          break;
+        }
+        _context4.n = 3;
+        return eventClient.GetUserEvents(userId);
+      case 3:
+        eventByUser = _context4.v;
+        console.log("\u0441\u043E\u0431\u044B\u0442\u0438\u044F \u0434\u043B\u044F \u043F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u0442\u0435\u043B\u044F");
+        console.log(eventByUser);
+        eventProfile.InsertDataToMainPage(eventByUser);
+      case 4:
+        if (!(UrlParts.includes("event") && UrlParts.includes('by-user') && UrlParts.includes(userId))) {
+          _context4.n = 6;
+          break;
+        }
+        _context4.n = 5;
+        return eventClient.GetUserEvents(userId);
+      case 5:
+        eventsByUser = _context4.v;
+        eventProfileProfile.EventListListByUserId(notificationsByUser, notificationClient.gatewayUrl);
+      case 6:
+        return _context4.a(2);
+    }
+  }, _callee4);
+})));
 
 /***/ }),
 
--- HPM-System/wwwroot/js/EventProfileManager.js
+++ HPM-System/wwwroot/js/EventProfileManager.js
@@ -6,6 +6,95 @@ import { DateFormat } from './DateFormat.js';
 
 export class NotificationProfileManager {
     constructor () {
+        this.houseProfile = new ApartmentHouses();
+        this.userId = window.authManager.userData.userId;
+    }
 
+    async InsertDataToCreateEvent() {
+        const houseSelector = document.getElementById('houseId');
+        const eventGroup = document.querySelector('.profile-group[data-group="event"]');
+
+        // Очистим селектор перед заполнением
+        houseSelector.innerHTML = '';
+
+        const houses = await this.houseProfile.GetHousesByUserId(this.userId);
+
+        const houseChecks = await Promise.all(
+            // Массив промисов для параллельного выполнения
+            houses.map(async (house) => {
+                const houseHead = await this.houseProfile.GetHead(house.id);
+                if (houseHead == null) return null;
+                return { house, isHead: houseHead.id === this.userId };
+            })
+        );
+
+        // Фильтруем только ненулевые значения, т.е. там где есть старший по дому
+        const results = houseChecks.filter(item => item !== null);
+
+        // Фильтруем дома, где пользователь — глава
+        const eligibleHouses = results
+            .filter(({ isHead }) => isHead)
+            .map(({ house }) => house);
+
+        if (eligibleHouses.length) {
+            // Пользователь — глава хотя бы в одном доме => показываем селект
+            eligibleHouses.forEach(house => {
+                const option = document.createElement('option');
+                option.value = house.id;
+                option.textContent = `${house.city}, ул. ${house.street}, ${house.number}`;
+                houseSelector.appendChild(option);
+            });
+        } else {
+            // Ни в одном доме не глава => событие недоступно
+            if (eventGroup) {
+                eventGroup.innerHTML = `Создание события недоступно`;
+            }
+        }
     }
 }
+
+document.addEventListener('authStateChanged', async () => {
+    const { isAuthenticated, userData } = event.detail;
+    const Regex = new window.RegularExtension();
+    const UrlParts = Regex.getUrlPathParts(window.location.href);
+
+    if (isAuthenticated && userData) {
+        const userId = window.authManager.userData.userId;
+        const eventProfile = new NotificationProfileManager();
+        const eventClient = new EventClient();
+
+        console.log('Аутентификация пройдена');
+
+        if (window.location.pathname.includes('/event/create')) {
+            await eventProfile.InsertDataToCreateEvent();
+
+            document.querySelector('[data-action="save-event-data"]').addEventListener('click', async () => {
+                console.log('Клик по кнопке сохранения уведомления');
+                
+                // Собираем данные уведомления
+                const eventData = await eventProfile.CollectEventDataToCreate();
+                console.log('Данные для сохранения:', eventData);
+                
+                //Отправляем данные на сервер
+                const eventCreate = eventClient.CreateEvent(eventData);
+
+                if (eventCreate) {                        
+                    Modal.ShowNotification('Событие создано успешно!', 'green');                        
+                }
+            });
+        }
+
+        if (window.location.pathname == '/') {            
+            const eventByUser = await eventClient.GetUserEvents(userId);
+            console.log(`события для пользователя`);
+            console.log(eventByUser);
+            eventProfile.InsertDataToMainPage(eventByUser);
+        }
+
+
+        if (UrlParts.includes(`event`) && UrlParts.includes('by-user') && UrlParts.includes(userId)) {
+            const eventsByUser = await eventClient.GetUserEvents(userId);            
+            eventProfileProfile.EventListListByUserId(notificationsByUser, notificationClient.gatewayUrl);
+        }
+    }
+});
